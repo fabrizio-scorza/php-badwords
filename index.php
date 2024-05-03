642c970476890d7995f741d4dfcca0660eb8efc9
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-03 15:16:05 +0200

    FIRST COMMIT - FEAT index and add server file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <!-- form per l'invio dei dati al server -->
+    <form action="./server.php">
+        <label for="message">Inserisci il tuo messaggio:</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <label for="badword">Inserisci la parola da censurare</label>
+        <input name="badword" id="badword" type="text">
+        <button>Invia</button>
+    </form>    
+</body>
+</html>
